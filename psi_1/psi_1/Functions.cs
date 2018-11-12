8b355f5b24672f6be2dfe464aa9f94d4c08be72e
Paweł Słonina <pawel.slonina@outlook.com>
2018-11-12 23:45:30 +0100

    zajecia #1

---
+++ psi_1/psi_1/Functions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psi_1
+{
+    public class Functions
+    {
+        public static List<double> NN(List<double> tabX, double W, double b) {
+            var listToReturn = new List<double>();
+            foreach (var x in tabX) {
+                listToReturn.Add(x * W + b);
+            }
+            return listToReturn;
+        }
+        public static double MSE(List<double> tabX, List<double> tabY) {
+            double suma = 0;
+            for (int i = 0; i < tabX.Count; i++)
+            {
+                suma += Math.Pow((tabX[i] - tabY[i]), 2);
+            }
+            return suma / tabX.Count;
+        }
+
+        public static List<double> ReLU(List<double> tabX) {
+            var listToReturn = new List<double>();
+            foreach(var x in tabX)
+            {
+                listToReturn.Add(Math.Max(0, x));
+            }
+            return listToReturn;
+        }
+
+        public static List<double> NN2(List<double> tabX, double W, double b, double W2, double b2) {
+            return NN(ReLU(NN(tabX, W, b)), W2, b2);
+        }
+    }
+}
---
+++ psi_1/psi_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using psi_1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psi_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tabX = new List<double>();
+            var random = new Random();
+            for (int i = 0; i < 100; i++) {
+                tabX.Add((random.NextDouble() * (2 - 0) + 0));
+            }
+            var tabY = new List<double>();
+            foreach (var value in tabX) {
+                tabY.Add(2 * value + 0.5);
+            }
+            var max = 3;
+            var min = -3;
+            var W = random.NextDouble() * Math.Abs(max - min) + min;
+            var b = random.NextDouble() * Math.Abs(max - min) + min;
+            var W2 = random.NextDouble() * Math.Abs(max - min) + min;
+            var b2 = random.NextDouble() * Math.Abs(max - min) + min;
+            var Z = Functions.NN(tabX, W, b);
+            Console.WriteLine(Functions.MSE(tabY, Z));
+            Console.WriteLine(Functions.MSE(tabY, Functions.NN(tabX, 2, 0.5)));
+            var Z2 = Functions.NN2(tabX, W, b, W2, b2);
+            Console.WriteLine(Functions.MSE(Z2, tabY));
+            Console.ReadLine();
+        }
+    }
+}
