257369236e01e42e7933a71c26b7f9e812c8e7bd
Paweł Słonina <pawel.slonina@outlook.com>
2018-11-13 00:15:51 +0100

    zaejcia #2

--- psi_1/psi_1/Functions.cs
+++ psi_1/psi_1/Functions.cs
@@ -36,5 +36,17 @@ namespace psi_1
         public static List<double> NN2(List<double> tabX, double W, double b, double W2, double b2) {
             return NN(ReLU(NN(tabX, W, b)), W2, b2);
         }
+
+        public static void Update(List<double> tabX, List<double> tabY, ref double W, ref double b, double E){
+            double dw, db;
+            dw = db = 0;
+            for (int i = 0; i < tabX.Count; i++)
+            {
+                dw = (W * tabX[i] + b - tabY[i] * tabX[i]) / tabX.Count;
+                db = (W * tabX[i] + b - tabY[i]) / tabX.Count;
+            }
+            W = W - E * dw;
+            b = b - E * db;
+        }
     }
 }
--- psi_1/psi_1/Program.cs
+++ psi_1/psi_1/Program.cs
@@ -31,6 +31,26 @@ namespace psi_1
             Console.WriteLine(Functions.MSE(tabY, Functions.NN(tabX, 2, 0.5)));
             var Z2 = Functions.NN2(tabX, W, b, W2, b2);
             Console.WriteLine(Functions.MSE(Z2, tabY));
+            var E = 0.01; //tempo uczenia
+            Console.WriteLine("====================================");
+            for (int i = 0; i < 1000; i++) {
+                Functions.Update(tabX, tabY, ref W, ref b, E);
+                Console.WriteLine(Functions.MSE(tabY, Functions.NN(tabX, W, b)));
+            }
+            var max2 = 0.1;
+            var min2 = -0.1;
+            double N; 
+            var Y2 = new List<double>();
+            var Y3 = new List<double>();
+            foreach (var x in tabX)
+            {
+                N = random.NextDouble() * Math.Abs(max2 - min2) + min2;
+                Y2.Add(-3 * x + 4 + N);
+            }
+            foreach (var y2 in Y2)
+            {
+                Y3.Add(Math.Pow(y2, 2));
+            }
             Console.ReadLine();
         }
     }
